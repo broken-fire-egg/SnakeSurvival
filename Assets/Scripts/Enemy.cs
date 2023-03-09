3eed1a2dee99cf2f3236eed530c18425026bd286
MurRuck <53210642+MurRuck@users.noreply.github.com>
2023-03-10 01:40:21 +0900

    2023-03-10
    적 움직임 추가

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,26 @@ using static EXPManager;
 public class Enemy : MonoBehaviour
 {
     public float hp;
+    public GameObject Player;
 
+    public enum Direction { right, down, left, up }
 
+    public Direction dir;
+    public bool test = false;
+    public float Speed;
+    void Start()
+    {
+        Player = GameObject.Find("Head");
+    }
+
+    private void Update()
+    {
+        if (test)
+            CheckLength();
+        else
+            CheckWidth();
+        //Move();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.transform.CompareTag("Player"))
@@ -16,6 +34,50 @@ public class Enemy : MonoBehaviour
         {
             EXPManager.instance.itemOP.GetRestingPoolObject().SetPositionAndActive(transform.position);
             Destroy(gameObject);
+        }
+    }
+
+    void CheckWidth()
+    {
+        if (Mathf.Round(Player.transform.position.x * 100) / 100 > Mathf.Round(transform.position.x * 100) / 100)
+            //dir = Direction.right;
+            transform.Translate(new Vector3(1, 0) * Speed);
+
+        else if (Mathf.Round(Player.transform.position.x * 100) / 100 == Mathf.Round(transform.position.x * 100) / 100)
+            test = true;
+        else
+            //dir = Direction.left;
+            transform.Translate(new Vector3(-1, 0) * Speed);
+    }
+    void CheckLength()
+    {
+        if (Mathf.Round(Player.transform.position.y * 100) / 100 > Mathf.Round(transform.position.y * 100) / 100)
+            //dir = Direction.up;
+            transform.Translate(new Vector3(0, 1) * Speed);
+
+        else if (Mathf.Round(Player.transform.position.y * 100) / 100 < Mathf.Round(transform.position.y * 100) / 100)
+            //dir = Direction.down;
+            transform.Translate(new Vector3(0, -1) * Speed);
+        else
+            test = false;
+    }
+
+    void Move()
+    {
+        switch (dir)
+        {
+            case Direction.right:
+                transform.Translate(new Vector3(1, 0) * Speed);
+                break;
+            case Direction.down:
+                transform.Translate(new Vector3(0, -1) * Speed);
+                break;
+            case Direction.left:
+                transform.Translate(new Vector3(-1, 0) * Speed);
+                break;
+            case Direction.up:
+                transform.Translate(new Vector3(0, 1) * Speed);
+                break;
         }
     }
 }
