b140009e9e485bb772b077fec6715bc9ad04fd68
broken-fire-egg <6716000@naver.com>
2023-05-17 21:28:47 +0900

    dotween 추가

--- Assets/Scripts/ObjectPools/CatWeaponObjectPool.cs
+++ Assets/Scripts/ObjectPools/CatWeaponObjectPool.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatWeaponObjectPool : ObjectPooling<GameObject>
+public class CatWeaponObjectPool : ObjectPooling<CapsuleCollider2D>
 {
     public static CatWeaponObjectPool instance;
     public void Start()
@@ -19,6 +19,7 @@ public class CatWeaponObjectPool : ObjectPooling<GameObject>
         var po = GetRestingPoolObject();
         var WeaponObject = po.gameObject;
         var weaponsr = po.spriteRenderer;
+        CapsuleCollider2D cc = po.component;
         switch (dir)
         {
             case SnakeHead.Direction.right:
@@ -26,24 +27,28 @@ public class CatWeaponObjectPool : ObjectPooling<GameObject>
                 WeaponObject.transform.localRotation = Quaternion.Euler(0f, 0f, 180f);
                 weaponsr.flipX = false;
                 weaponsr.flipY = true;
+                cc.offset = new Vector2();
                 break;
             case SnakeHead.Direction.down:
                 WeaponObject.transform.localPosition = pos;
                 WeaponObject.transform.localRotation = Quaternion.Euler(0f, 0f, -90f);
                 weaponsr.flipX = true;
                 weaponsr.flipY = false;
+                cc.offset = new Vector2();
                 break;
             case SnakeHead.Direction.left:
                 WeaponObject.transform.localPosition = pos;
                 WeaponObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
                 weaponsr.flipX = false;
                 weaponsr.flipY = false;
+                cc.offset = new Vector2();
                 break;
             case SnakeHead.Direction.up:
                 WeaponObject.transform.localPosition = pos;
                 WeaponObject.transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
                 weaponsr.flipX = true;
                 weaponsr.flipY = true;
+                cc.offset = new Vector2();
                 break;
         }
         po.SetPositionAndActive(pos);
